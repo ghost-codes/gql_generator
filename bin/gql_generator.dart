1c442ab899c2b11bd7a385cdc833e7dc1bcb371b
Hope Dorkenoo <compounddork@gmail.com>
2022-11-29 15:51:24 +0000

    add terminal loader

--- bin/gql_generator.dart
+++ bin/gql_generator.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:gql_generator/src/dataProcessor.dart';
+import 'package:interact/interact.dart';
 import 'package:yaml/yaml.dart';
 
 Future<void> main(List<String> args) async {
@@ -11,8 +12,15 @@ Future<void> main(List<String> args) async {
     throw "Please provide graphql link in order to generate files";
   }
   final link = args[indexOfFileLink];
-
+  final loading = Spinner(
+    icon: '🏆',
+    leftPrompt: (done) => '', // prompts are optional
+    rightPrompt: (done) => done
+        ? 'here is a trophy for being patient'
+        : 'searching a thing for you',
+  ).interact();
   final result = await DataProcessor.fetchSchema(link);
   File file = File("../graphql_meta_data.json");
   await file.writeAsString(json.encode(result));
+  loading.done();
 }
