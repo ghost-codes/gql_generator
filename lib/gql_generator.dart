c267640bf842da9275f6008ce847c1aba76191d1
Hope Dorkenoo <compounddork@gmail.com>
2022-11-29 15:46:41 +0000

    prep for code generation

--- lib/gql_generator.dart
+++ lib/gql_generator.dart
@@ -1,7 +1,17 @@
 library gql_generator;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:io';
+
+import 'package:build/build.dart';
+import 'package:gql_generator/src/modelBuilder.dart';
+
+Builder gqlGenerator(BuilderOptions options) {
+  // throw UnresolvableAssetException("Network call on  return error of ");
+
+  //  options.
+  //  final file = File( "graphql_config.yaml");
+  //  if(!(await file.exists()))
+  //   final doc = loadYaml("", sourceUrl: Uri.parse("./graphql.yaml"));
+  //   print(doc);
+  return ModelBuilder();
 }
