a830128733163885139694670e8041d67436733b
Hope Dorkenoo <compounddork@gmail.com>
2022-11-30 23:05:26 +0000

    clean up and code generation template setup

--- lib/gql_generator.dart
+++ lib/gql_generator.dart
@@ -1,17 +1,3 @@
 library gql_generator;
 
 import 'dart:io';
-
-import 'package:build/build.dart';
-import 'package:gql_generator/src/modelBuilder.dart';
-
-Builder gqlGenerator(BuilderOptions options) {
-  // throw UnresolvableAssetException("Network call on  return error of ");
-
-  //  options.
-  //  final file = File( "graphql_config.yaml");
-  //  if(!(await file.exists()))
-  //   final doc = loadYaml("", sourceUrl: Uri.parse("./graphql.yaml"));
-  //   print(doc);
-  return ModelBuilder();
-}
